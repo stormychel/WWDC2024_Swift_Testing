ffb241b944a44ee7711c3daef50078536005506d
Michel Storms <stormychel@me.com>
2024-06-18 22:19:08 +0200

    - struct with stored properties

--- WWDC2024_Swift_TestingTests/WWDC2024_Swift_TestingTests.swift
+++ WWDC2024_Swift_TestingTests/WWDC2024_Swift_TestingTests.swift
@@ -9,27 +9,27 @@ import Testing
 
 struct WWDC2024_Swift_TestingTests {
 
-    @Test("testExpect") func testExpect() async throws {
-        print("testExpect()")
-        
-        let expected: Int = 36
+    struct TestingTests {
         let number: Int = 3
         let multiplier: Int = 12
         
-        #expect(number * multiplier == expected)
-    }
-
-    @Test("testRequire") func testRequire() async throws {
-        print("testRequire()")
-  
-//        let expectedOptionals: [Int?] = [] // Expectation failed: (expectedOptionals → []).first → nil → nil
-        let expectedOptionals: [Int?] = [36, nil, 4] // Passes test
+        @Test("testExpect") func testExpect() async throws {
+            print("testExpect()")
+            
+            let expected: Int = 36
+            
+            #expect(number * multiplier == expected)
+        }
         
-        let expected = try #require(expectedOptionals.first)
-        
-        let number: Int = 3
-        let multiplier: Int = 12
-        
-        #expect(number * multiplier == expected)
+        @Test("testRequire") func testRequire() async throws {
+            print("testRequire()")
+            
+            //        let expectedOptionals: [Int?] = [] // Expectation failed: (expectedOptionals → []).first → nil → nil
+            let expectedOptionals: [Int?] = [36, nil, 4] // Passes test
+            
+            let expected = try #require(expectedOptionals.first)
+            
+            #expect(number * multiplier == expected)
+        }
     }
 }
