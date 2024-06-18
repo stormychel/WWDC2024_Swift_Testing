42d53fff2b091c73d04a68f5f4aff8f05ea7974e
Michel Storms <stormychel@me.com>
2024-06-18 22:01:52 +0200

    - simple math test

--- WWDC2024_Swift_TestingTests/WWDC2024_Swift_TestingTests.swift
+++ WWDC2024_Swift_TestingTests/WWDC2024_Swift_TestingTests.swift
@@ -10,7 +10,13 @@ import Testing
 struct WWDC2024_Swift_TestingTests {
 
     @Test func testExample() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        print("testExample()")
+        
+        let expected: Int = 36
+        let number: Int = 3
+        let multiplier: Int = 12
+        
+        #expect(number * multiplier == expected)
     }
 
 }
